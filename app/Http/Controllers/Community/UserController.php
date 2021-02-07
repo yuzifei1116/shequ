78904386d057b43276b2f6eb9de330c92fed6cd9
yuzifei <1213649307@qq.com>
2021-02-07 09:12:13 +0800

    1

--- app/Http/Controllers/Community/UserController.php
+++ app/Http/Controllers/Community/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
 
             $page   = $page * $limit;
 
-            $data = \App\Product::where('user_id',$request->user->id)->where('server',1)->offset($page)->limit($limit)->get();
+            $data = \App\Product::where('user_id',$request->user->id)->where('server',1)->orderBy('id','desc')->offset($page)->limit($limit)->get();
 
             if($data){
 
