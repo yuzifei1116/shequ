0bc81b09b9611194f9bd8e703533c128202782fe
yuzifei <1213649307@qq.com>
2021-02-07 09:09:09 +0800

    1

--- app/Http/Controllers/Community/ProductController.php
+++ app/Http/Controllers/Community/ProductController.php
@@ -49,46 +49,6 @@ class ProductController extends Controller
     }
 
     /**
-     * 发布转让产品
-     */
-    public function proCreate(Request $request)
-    {
-        try {
-            
-            $data = array();
-
-            if(!$request->name) return error('请填写产品名称'); else $data['name'] = $request->name;
-
-            if(!$request->rate_cate) return error('请填写收益分配方式'); else $data['rate_cate'] = $request->rate_cate;
-
-            if(!$request->annualized) return error('请填写预计年化'); else $data['annualized'] = $request->annualized;
-
-            if(!$request->turn_money) return error('请填写金额'); else $data['turn_money'] = $request->turn_money;
-
-            if(!$request->end_time) return error('请填写预计到期日期'); else $data['end_time'] = strtotime($request->end_time);
-
-            if(!$request->suv_day) return error('请填写剩余天数'); else $data['suv_day'] = $request->suv_day;
-
-            if(!$request->cate_id) return error('请选择分类'); else $data['cate_id'] = $request->cate_id;
-
-            if($request->remark) $data['remark'] = $request->remark;
-            
-            $data['phone'] = phone();
-            
-            $data['user_id'] = $request->user->id;
-
-            $result = \App\Product::create($data);
-
-            if($result) return result('发布成功');
-
-        } catch (\Throwable $th) {
-            
-            return error();
-
-        }
-    }
-
-    /**
      * 产品列表
      */
     public function product(Request $request)
@@ -96,7 +56,7 @@ class ProductController extends Controller
         try {
             
             //发布产品列表
-            $data['fa_product'] = \App\Product::where('is_site',0)->where('is_show',1)->where('server',1)->orderby('id','desc')->get();
+            $data['fa_product'] = \App\Product::where('is_site',0)->where('is_show',1)->where('server',1)->orderBy('id','desc')->get();
 
             if($data['fa_product']){
 
